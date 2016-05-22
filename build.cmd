e648d549119d2daed87bca8ffae4ef9a993a707f
valentingalea <valentingalea@gmail.com>
2016-05-22 20:44:05 +0100

    cleanup

--- build.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set OUT=Valentin-Galea
-
-if exist %OUT%.pdf del /Q %OUT%.pdf
-
-pandoc CV.md -o %OUT%.pdf --from=markdown_github --variable mainfont=Arial --variable sansfont=Arial --variable monofont="Courier New" --variable fontsize=10pt --latex-engine=xelatex
-
-if exist %OUT%.pdf start %OUT%.pdf
